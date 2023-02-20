7c217b1b6aec5d71a3cf26cf54cf800cb0ab6282
SamLCH <64434536+sam-vic@users.noreply.github.com>
2023-02-20 08:54:35 -0800

    feat: 1) add button adds value to an ol 2) remove btn for incorrect value 3) complete btn for task done 4) placeholder for empty input value

--- js/script.js
+++ js/script.js
@@ -1,1 +1,43 @@
-let inputValue = $('#input').val()
+let list = $('#list')
+
+// function to add input to list
+let addToList = () => {
+  let inputValue = $('#input').val()
+  // append input values in li
+  let li = $('<li></li>')
+  let text = $('<span></span>')
+  // incase value is empty
+  if (inputValue === '') {
+      $('#input').attr('placeholder', 'Please enter a value!')
+      return
+  }
+  text.append(inputValue)
+  li.append(text)
+  list.append(li)
+
+  // add remove button
+  let removeBtn = $('<button></button>')
+  removeBtn.text('Remove')
+  removeBtn.click(() => {
+      li.remove()
+  });
+  li.append(removeBtn)
+
+  //cross out item when done but not removed
+  let crossItm = $('<button></button>')
+  crossItm.text('Complete')
+  crossItm.click(() => {
+      li.addClass('strike')
+  })
+  li.append(crossItm)
+
+  $('#input').val('')
+}
+
+$('.add-button').click(addToList)
+
+window.addEventListener('keydown', event => {
+  if (event.key === 'Enter') {
+     addToList()
+  }
+})
